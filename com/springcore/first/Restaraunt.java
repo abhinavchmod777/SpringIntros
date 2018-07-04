ad0e2ed9daa2b21c99cc21ccf8b8c3f42224d040
abhinavchmod777 <abhinav.26aug@gmail.com>
2018-07-04 13:00:13 +0530

    Beans Scope

--- com/springcore/first/Restaraunt.java
+++ com/springcore/first/Restaraunt.java
@@ -1,17 +1,15 @@
 package com.springcore.first;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 public class Restaraunt 
 {
-	Collection<String> waitersList = new ArrayList<String>();
-
-	public void setWaitersList(Collection<String> waitersList) {
-		this.waitersList = waitersList;
+	private String welcomeNote;
+	
+	public void setWelcomeNote(String welcomeNote) {
+		this.welcomeNote = welcomeNote;
 	}
-	public void displayWaitersList(){
-		System.out.println("Waiters list = "+ waitersList);
+
+	public void greetCustomer() {
+		System.out.println(welcomeNote);
 	}
 	
 }
--- com/springcore/first/Test.java
+++ com/springcore/first/Test.java
@@ -5,10 +5,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test 
 {
-	public static void main(String args[]) {
+	public static void main(String args[]) 
+	{
 		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 		
-		Restaraunt obj = (Restaraunt)context.getBean("restarauntBean");
-		obj.displayWaitersList();
+		Restaraunt obj1 = (Restaraunt)context.getBean("restarauntBean");
+		obj1.setWelcomeNote("Object1 welcome note");
+		obj1.greetCustomer();
+		
+		Restaraunt obj2 = (Restaraunt)context.getBean("restarauntBean");
+		obj2.greetCustomer();
 	}
 }
